917411dd6fdc4462aaf4ab9d31df74cd1343b5fb
Tung <tungduong2288@gmail.com>
2024-09-14 14:26:53 +0700

    base completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/base/bottom_nav_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: BottomNavBar(),
     );
   }
 }
